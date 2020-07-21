9444ad2c47338ee077734835e9c515301b15d1ac
Shabab <shababsaifi@gmail.com>
2020-07-21 18:43:45 +0530

    added loadContact function

---
+++ src/contact.js
@@ -0,0 +1,10 @@
+const loadContact = (element) => {
+  element.innerHTML = `<h2>Contact</h2>
+  <hr>
+  <h4>Address</h4>
+  <p>Cannaught Place</p>
+  <p>Shop No. 001</p>
+  <p>New Delhi, India`;
+};
+
+export default loadContact;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import initialPageLoad from './initialPageLoad';
 
 import loadMenu from './menu';
 
+import loadContact from './contact';
+
 const contentDiv = document.querySelector('#content');
 const tabs = document.querySelectorAll('.tab');
+const removeClass = (element, className) => {
+  element.classList.remove(className);
+};
+
+const addClass = (element, className) => {
+  element.classList.add(className);
+};
 const clickHandler = (tab) => {
-  if (tab.innerText === 'Menu') {
-    loadMenu(contentDiv);
-  }
+  tabs.forEach(tab => removeClass(tab, 'active'));
   if (tab.innerText === 'Home') {
     initialPageLoad(contentDiv);
+    addClass(tab, 'active');
+  }
+  if (tab.innerText === 'Menu') {
+    loadMenu(contentDiv);
+    addClass(tab, 'active');
+  }
+  if (tab.innerText === 'Contact') {
+    loadContact(contentDiv);
+    addClass(tab, 'active');
   }
 };
 
